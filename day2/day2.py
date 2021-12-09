4c4b52925fdda771224dbc2c5e5f3fdf38669f23
Eduardo Villota <eduardouio7@gmail.com>
2021-12-09 15:23:11 -0500

    Segunda parte dia2

--- day2/day2.py
+++ day2/day2.py
@@ -2,8 +2,7 @@ import os
 from unittest import result
 
 def get_location(data)->int:
-    x = 0
-    y = 0
+    x = y = 0
     for item in data:
         coordinates = item.split(' ')
         if coordinates[0].lower() == 'forward':
@@ -17,7 +16,28 @@ def get_location(data)->int:
         
     return (x*y)
 
-path = os.getcwd() + '/day3/input_data.txt'
+def get_location_2(data)->int:
+    x = obj = dep = 0
+    for item in data:
+        coordinates = item.split(' ')
+        if coordinates[0].lower() == 'forward':
+            x += int(coordinates[1])
+            dep = (int(coordinates[1]) * obj) + dep
+
+        if coordinates[0].lower() == 'up':
+            obj -= int(coordinates[1])
+
+        if coordinates[0].lower() == 'down':
+            obj += int(coordinates[1])    
+    
+    return (x * dep)
+
+path = os.getcwd() + '/day2/input_data.txt'
 input_data = open(path, 'r').read().splitlines()
 
 print(get_location(input_data))
+print(get_location_2(input_data))
+
+
+
+
