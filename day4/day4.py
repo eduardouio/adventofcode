99e0080c2826975350f4ca4618b4ddb92386d182
= <=>
2021-12-23 00:29:36 -0500

    Primer intento de armar bingo

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,64 @@
+from more_itertools import chunked
 
-def base_bingo_data(input_data):
-    return 1,[[],[],[]]
+
+class Bingo():
+
+    def __init__(self, input_data):
+        self.tables = []
+        self.shadow_tables = []
+        self.entries = []
+
+        data = input_data.split('\n')
+        entries = data[0].rstrip().split(',')
+        self.entries = [int(x) for x in entries]
+        del(data[0])
+        data = [d for d in data if d]
+        data = list(chunked(data, 5))
+
+        for line in data:
+            table = []
+            s_table = []
+            for row in line:
+                values_row = row.split(' ')
+                table.append([int(v) for v in values_row if v])
+
+            self.tables.append(table)
+
+        for tbl in self.tables:
+            table = [[0] * len(_) for _ in tbl]
+            self.shadow_tables.append(table)
+    
+    def mark_shadow(self, entry):
+        for tbl, table in enumerate(self.tables):
+            for rw, row in enumerate(table):
+                if entry in row:
+                    col = row.index(entry)
+                    self.shadow_tables[tbl][rw][col] = 1
+    
+    def run_entries(self):
+        for entry in self.entries:
+            self.mark_shadow(entry)
+            if self.bingo():
+                return self.get_value()
+        
+        return False
+
+    def bingo(self):
+        for table in self.shadow_tables:
+            for row in table:
+                if all(row):
+                    return True
+                
+        columns = [list(x) for x in zip(*self.shadow_tables)]
+        columns = [[_[0]][0] for _ in columns]
+        
+        for col in columns:
+                if all(col):
+                    return True
+        
+        import ipdb; ipdb.set_trace()
+        return False
+    
+    
+    def get_value(self):
+        pass
--- day4/test.py
+++ day4/test.py
@@ -1,8 +1,7 @@
 import unittest
-from day4 import base_bingo_data
+from day4 import Bingo
 
-base_data = """
-7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+base_data = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
  8  2 23  4 24
@@ -28,47 +27,135 @@ class TestDay4(unittest.TestCase):
     def setUp(self) -> None:
         self.base_data = base_data
         self.bingo_entries = [
-            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15,
-            25, 12, 22, 18, 20, 8, 19, 3, 26, 1
+            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1
         ]
         self.bingo_tables = [
-            [22, 13, 17, 11, 0,
-             8, 2, 23, 4, 24,
-             21, 9, 14, 16, 7,
-             6, 10, 3, 18, 5,
-             1, 12, 20, 15, 19],
-            [3, 15, 0, 2, 22,
-             9, 18, 13, 17, 5,
-             19, 8, 7, 25, 23,
-             20, 11, 10, 24, 4,
-             14, 21, 16, 12, 6],
-            [14, 21, 17, 24, 4,
-             10, 16, 15, 9, 19,
-             18, 8, 23, 26, 20,
-             22, 11, 13, 6, 5,
-             2, 0, 12, 3, 7,]
+            [
+                [22, 13, 17, 11, 0],
+                [8, 2, 23, 4, 24],
+                [21, 9, 14, 16, 7],
+                [6, 10, 3, 18, 5],
+                [1, 12, 20, 15, 19]
+             ],
+            [
+                [3, 15, 0, 2, 22,],
+                [9, 18, 13, 17, 5,],
+                [19, 8, 7, 25, 23,],
+                [20, 11, 10, 24, 4,],
+                [14, 21, 16, 12, 6],
+            ],
+            [
+                [14, 21, 17, 24, 4,],
+                [10, 16, 15, 9, 19,],
+                [18, 8, 23, 26, 20,],
+                [22, 11, 13, 6, 5,],
+                [2, 0, 12, 3, 7,],
+            ]
         ]
-        return super().setUp()
+        self.bingo = Bingo(self.base_data)
+        return self.bingo
 
     def test_base_bingo_data(self):
-        entries, tables = base_bingo_data(self.base_data)
-        self.assertListEqual(self.bingo_entries, entries)
-        self.assertListEqual(self.bingo_tables, tables)
         
+        self.assertListEqual(self.bingo_entries, self.bingo.entries)
+        
+        for k,item in enumerate(self.bingo_tables):
+            self.assertListEqual(self.bingo_tables[k], self.bingo.tables[k])
+        
+        self.assertListEqual([[[0] * 5] * 5] *3 , self.bingo.shadow_tables)
+        
+    def test_entrie_step_1(self):
+        spected_shadow = [
+            [
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 1],
+                [0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1],
+                [0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 1],
+                [0, 0, 0, 0, 0],
+            ],
+            [
+                [0, 0, 0, 0, 1],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 1],
+                [0, 0, 0, 0, 1],
+            ]
+        ]
+        self.bingo.entries = self.bingo_entries[:5]
+        self.bingo.run_entries()
+        self.assertListEqual(spected_shadow, self.bingo.shadow_tables)
     
-    def test_make_table(self):
-        pass
-    
-    def test_vertical_line_bingo(self):
-        pass
-    
-    def test_column_line_bingo(self):
-        pass
-    
-    def test_bingo_game(self):
-        pass
+    def test_entrie_step_2(self):
+        spected_shadow = [
+            [
+                [0, 0, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 0, 1],
+                [0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 1, 1, 0],
+                [1, 0, 0, 1, 1],
+                [0, 0, 1, 0, 1],
+                [0, 1, 0, 0, 1],
+                [1, 1, 0, 0, 0],
+            ],
+            [
+                [1, 1, 1, 0, 1],
+                [0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 1],
+                [1, 1, 0, 0, 1],
+            ]
+        ]
+        self.bingo.entries = self.bingo_entries[:11]
+        self.bingo.run_entries()
+        self.assertListEqual(spected_shadow[0][0], self.bingo.shadow_tables[0][0])
 
+    def test_bingo(self):
+        shadow_false = [
+            [
+                [0, 0, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 0, 1],
+                [0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0]
+            ]
+        ]
 
+        shadow_true_vertical = [
+            [
+                [0, 0, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 1, 0],
+                [0, 0, 1, 0, 1],
+                [0, 0, 1, 0, 0]
+            ]
+        ]
+        
+        shadow_true_horizontal = [
+            [
+                [0, 0, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 0, 1],
+                [0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1]
+            ]
+        ]
+        
+        self.bingo.shadow_tables = shadow_false
+        self.assertFalse(self.bingo.bingo())
+        
+        self.bingo.shadow_tables = shadow_true_vertical
+        self.assertTrue(self.bingo.bingo())
 
 if __name__ == '__main__':
     unittest.main()
